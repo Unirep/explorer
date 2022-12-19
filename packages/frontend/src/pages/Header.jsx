4575817022587a0195d21179dbc0eafe4d1ead8f
Colleen Rose <cjrose59@gmail.com>
2022-12-19 12:51:13 -0700

    add header elements

--- packages/frontend/src/pages/Header.jsx
+++ packages/frontend/src/pages/Header.jsx
@@ -8,9 +8,13 @@ export default () => {
             <div className="header">
                 <img src={require('../../public/logo.svg')} alt="UniRep logo"/>
                 <div className="links">
-                    <a href="https://developer.unirep.io/" target='blank'>Docs</a>
-                    <a href="https://github.com/Unirep" target='blank'>GitHub</a>
-                    <a href="https://discord.com/invite/VzMMDJmYc5" target='blank'>Discord</a>
+                    <a className="link-item" href="https://developer.unirep.io/" target='blank'>Docs</a>
+                    <a className="link-item" href="https://github.com/Unirep" target='blank'><img src={require('../../public/github.svg')} alt="GitHub logo"/></a>
+                    <a className="link-item" href="https://discord.com/invite/VzMMDJmYc5" target='blank'><img src={require('../../public/discord.svg')} alt="Discord logo"/></a>
+                    <div className="link-item" >
+                     <img src={require('../../public/sun_icon.svg')} alt="sun icon"/>
+                    </div>
+                    <button className="link-item" style={{background: "white", borderRadius: "24px", padding: "6px 30px"}}>Build</button>
                 </div>
             </div>
 
