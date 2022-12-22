8bfd5c6a322b3c58bc6f051c0f009dc9213f291b
Colleen Rose <cjrose59@gmail.com>
2022-12-22 14:15:16 -0700

    fix header/buttons

--- packages/frontend/src/components/Footer.jsx
+++ packages/frontend/src/components/Footer.jsx
@@ -6,12 +6,12 @@ export default () => {
     return (
         <div className="footer flex" style={{backgroundImage: `url(${Background})`}}>
             <div className="flex">
-                <a href="https://developer.unirep.io/" target='blank' style={{marginRight: "2em"}}>Docs</a>
+                <a href="https://developer.unirep.io/" target='blank' className="link">Docs</a>
                 <a href="https://discord.com/invite/VzMMDJmYc5" target='blank'>Support</a>
             </div>
             <img src={require('../../public/logo.svg')} alt="UniRep logo"/>
             <div className="flex">
-                <a href="https://github.com/Unirep" target='blank' style={{marginRight: "2em"}}><img src={require('../../public/github.svg')} alt="GitHub logo"/></a>
+                <a href="https://github.com/Unirep" target='blank' className="link"><img src={require('../../public/github.svg')} alt="GitHub logo"/></a>
                 <a  href="https://github.com/Unirep/create-unirep-app" target='blank'><button>Build your own</button></a>
             </div>
         </div>
--- packages/frontend/src/components/Header.jsx
+++ packages/frontend/src/components/Header.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import './header.css'
 
 export default () => {
     return (
         <>
             <div className="header">
-                <img src={require('../../public/logo.svg')} alt="UniRep logo"/>
-                <div className="links">
-                    <a className="link-item" href="https://developer.unirep.io/" target='blank'>Docs</a>
-                    <a className="link-item" href="https://github.com/Unirep" target='blank'><img src={require('../../public/github.svg')} alt="GitHub logo"/></a>
-                    <a className="link-item" href="https://discord.com/invite/VzMMDJmYc5" target='blank'><img src={require('../../public/discord.svg')} alt="Discord logo"/></a>
-                    <div className="link-item" >
+                <Link to='/'><img src={require('../../public/logo.svg')} alt="UniRep logo"/></Link>
+                <div className="flex">
+                    <a className="link" href="https://developer.unirep.io/" target='blank'>Docs</a>
+                    <a className="link" href="https://github.com/Unirep" target='blank'><img src={require('../../public/github.svg')} alt="GitHub logo"/></a>
+                    <a className="link" href="https://discord.com/invite/VzMMDJmYc5" target='blank'><img src={require('../../public/discord.svg')} alt="Discord logo"/></a>
+                    <div className="link" >
                      <img src={require('../../public/sun_icon.svg')} alt="sun icon"/>
                     </div>
-                    <button className="link-item" style={{background: "white", borderRadius: "24px", padding: "6px 30px"}}>Build</button>
+                    <a href="https://github.com/Unirep/create-unirep-app" target='blank'><button>Build</button></a>
                 </div>
             </div>
 
