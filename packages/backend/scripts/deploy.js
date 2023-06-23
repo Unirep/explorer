50662c6cfcdb8ef54b03b9fd7d19f95fe784bef3
bhargav <bhargav.annem@gmail.com>
2023-06-23 14:16:49 -0500

    lint

--- packages/backend/scripts/deploy.js
+++ packages/backend/scripts/deploy.js
@@ -41,9 +41,7 @@ async function main() {
   console.log('Deploying contracts with the account:', deployer.address)
   const token = await ethers.deployContract('AttesterDescription')
 
-  // console.log('Token address:', await token.getAddress())
-  console.log(token)
-  console.log(await token.helloWorld())
+  console.log('Token address:', await token.getAddress())
 }
 
 main()
