00f92cc3a5da61f1e86f5f141a3da459fde1c802
bhargav <bhargav.annem@gmail.com>
2023-07-03 17:25:35 -0500

    feat(backend): set description with signature

--- packages/backend/src/routes/attesterDescription.mjs
+++ packages/backend/src/routes/attesterDescription.mjs
@@ -10,10 +10,10 @@ export default ({ app, db, synchronizer }) => {
     // isValidSignatureSelector 0xe0c5e6c3
     // getDescriptionSelector 0x1a092541
     // setDescriptionSelector 0xf0d3533b
-    // interfaceId 0x0a1f90b9
+    // interfaceId 0x93c93c46
 
     const attesterId = req.params.attesterId
-    const token = req.headers.token
+    const nonce = req.headers.nonce
     const { icon, url, name, description } = req.headers
 
     if (!isValidAttesterDescription(icon, name, description, url)) {
@@ -23,8 +23,8 @@ export default ({ app, db, synchronizer }) => {
 
     let attesterDescription = JSON.stringify({ icon, name, description, url })
 
-    const signer = localProvider.getSigner()
-    // const signer = provider.getSigner()
+    // const signer = localProvider.getSigner()
+    const signer = provider.getSigner()
     const contract = new ethers.Contract(
       attesterId,
       attesterDescriptionAbi,
@@ -33,19 +33,17 @@ export default ({ app, db, synchronizer }) => {
 
     const supportsInterface =
       typeof contract.supportsInterface === 'function'
-        ? await contract.supportsInterface('0x0a1f90b9')
+        ? await contract.supportsInterface('0x93c93c46')
         : false
 
     let validSignature = false
 
-    const hash = ethers.utils.keccak256(
-      ethers.utils.solidityPack(
-        ['uint256', 'string', 'address'],
-        [token, description, attesterId]
-      )
+    const hash = ethers.utils.solidityKeccak256(
+      ['uint256', 'string'],
+      [nonce, description]
     )
 
-    const signature = ethers.utils.keccak256(hash)
+    const signature = await signer.signMessage(ethers.utils.arrayify(hash))
 
     if (supportsInterface) {
       let _ = await contract.setDescription(
