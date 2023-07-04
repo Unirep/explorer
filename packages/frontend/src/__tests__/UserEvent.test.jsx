3818f6f806022b3247f92f92fa9da1afa029309d
yu-zhen <wlphg010688@gmail.com>
2023-07-04 21:21:43 +0100

    UserEvent tests

---
+++ packages/frontend/src/__tests__/UserEvent.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import fetch from 'whatwg-fetch'
+import UserEvent from '../components/UserEvent'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  Link: jest.fn(),
+}))
+
+const defaultStateData = {
+  attester: {
+    signUpsById: new Map(),
+  },
+}
+
+beforeAll(() => {
+  defaultStateData.attester.signUpsById.set(123, {
+    commitment: '123',
+    epoch: 1,
+  })
+})
+
+test('To test if UnirepEvent is exactly rendered', async () => {
+  const { container } = render(
+    <UserEvent
+      signup={{ attesterId: '123', epoch: 1, timestamp: 1688393495 }}
+    />
+  )
+
+  expect(container.querySelector('.event-card')).not.toBeNull()
+})
