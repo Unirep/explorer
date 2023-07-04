ab129c415f9afef0e836fa9ebef2b6ca5df7c0f8
yu-zhen <wlphg010688@gmail.com>
2023-07-04 18:23:08 +0100

    Footer tests

---
+++ packages/frontend/src/__tests__/Footer.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import fetch from 'whatwg-fetch'
+import Footer from '../components/Footer'
+
+test('To test if NotFoundPage is exactly rendered', async () => {
+  render(<Footer />)
+
+  expect(screen.getByText('Docs')).toBeInTheDocument()
+})
