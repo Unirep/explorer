156e892bd177fba24022c99de4e434fe3a2aa7e9
bhargav <bhargav.annem@gmail.com>
2023-07-27 21:47:26 -0500

    fix: linting

--- packages/backend/hardhat.config.js
+++ packages/backend/hardhat.config.js
@@ -5,17 +5,22 @@ module.exports = {
   networks: {
     hardhat: {
       blockGasLimit: 12000000,
+      chainId: 421613,
     },
     local: {
       url: 'http://localhost:8545',
       blockGasLimit: 12000000,
+      chainId: 421613,
       accounts: [
         '0xbca43b51928100e446f2265f1493c3e9187d62eb26ef043f8ecbae6cb27e4f51',
       ],
     },
     arb: {
       url: 'https://arb-goerli.g.alchemy.com/v2/hfFfXlX8rR8YvrALiJ8b7ZtIPRGY1GTM',
-      accounts: [],
+      accounts: [
+        '0xbca43b51928100e446f2265f1493c3e9187d62eb26ef043f8ecbae6cb27e4f51',
+        '0x0f70e777f814334daa4456ac32b9a1fdca75ae07f70c2e6cef92679bad06c88b',
+      ],
     },
   },
   solidity: {
