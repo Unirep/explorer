519a8e73f8e972220b5451ce79969d96fe2e3af6
bhargav <bhargav.annem@gmail.com>
2023-08-04 09:06:47 -0500

    refactor(backend): fix network names

--- packages/backend/test/blockExplorer.test.mjs
+++ packages/backend/test/blockExplorer.test.mjs
@@ -1,23 +1,23 @@
 import { BlockExplorer, getDeployer } from '../src/helpers/blockExplorer.mjs'
 import { expect } from 'chai'
 export const CONTRACT_DEPLOYERS = {
-  Polygon: {
+  polygon: {
     address: '0x3f2f4aa023c6ed149d342229afac6e140e149114',
     deployer: '0x7356723b52a3a6a7c9858937b8c82d9480cfd513',
   },
-  Sepolia: {
+  sepolia: {
     address: '0x6375394335f34848b850114b66A49D6F47f2cdA8',
     deployer: '0x19B4F9C381C7927FE33D853e48b560141A380C44',
   },
-  Mainnet: {
+  mainnet: {
     address: '0x1a0ad011913A150f69f6A19DF447A0CfD9551054',
     deployer: '0xEe729F57F0111FD0F660867d0F522f983202a5aF',
   },
-  Goerli: {
+  goerli: {
     address: '0x17e1cbB275642A465Af102fDcb7dABdd5B413A6d',
     deployer: '0x6Ed13984664B3A2B7A46d3646dcB523d927839aa',
   },
-  ArbitrumGoerli: {
+  'arbitrum-goerli': {
     address: '0xac836aa647477d98b148a97d6437bddc35cc9f77',
     deployer: '0x20f3dfc8513b9482455943153e53c11062810b45',
   },
