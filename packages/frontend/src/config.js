ec80e899f243a6cc7ba7942242a7679d2f43d3df
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-09-06 18:37:03 +0800

    build(frontend): use env server, add favicon path (#77)

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -3,6 +3,4 @@ export const network = 'arbitrum-goerli'
 // arbitrum-goerli, goerli, mumbai, sepolia
 
 // in CI we append a change to this value
-let SERVER = 'http://127.0.0.1:8000'
-// let SERVER = 'https://api.explorer.unirep.io'
-export { SERVER }
+export const SERVER = process.env.SERVER ?? 'http://127.0.0.1:8000'
