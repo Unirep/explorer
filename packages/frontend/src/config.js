0d2a56821228aa8c0564603f9f988713aa6d041f
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-09-13 11:13:23 +0800

    fix: remove setting SERVER url to frontend config

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -3,4 +3,6 @@ export const network = 'arbitrum-goerli'
 // arbitrum-goerli, goerli, mumbai, sepolia
 
 // in CI we append a change to this value
-export const SERVER = process.env.SERVER ?? 'http://127.0.0.1:8000'
+let SERVER = 'http://127.0.0.1:8000'
+// let SERVER = 'https://api.explorer.unirep.io'
+export { SERVER }
