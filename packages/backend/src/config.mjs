fffb3fc618a3e8f9e6b28e9f64da721c362cfb86
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-01-08 16:36:22 +0800

    chore: add networks

--- packages/backend/src/config.mjs
+++ packages/backend/src/config.mjs
@@ -7,13 +7,20 @@ config()
 export const DB_PATH = process.env.DB_PATH ?? ':memory:'
 
 export const NETWORK = {
-  // 'arbitrum-goerli': {
-  //   provider: new ethers.providers.JsonRpcProvider(
-  //     `https://arbitrum-goerli.infura.io/v3/${process.env.INFURA_KEY}`
-  //   ),
-  //   explorer: 'https://goerli.arbiscan.io',
-  //   unirepAddress: '0x4D137bb44553d55AE6B28B5391c6f537b06C9cc3',
-  // },
+  sepolia: {
+    provider: new ethers.providers.JsonRpcProvider(
+      `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`
+    ),
+    explorer: 'https://sepolia.etherscan.io',
+    unirepAddress: '0x83cB6AF63eAfEc7998cC601eC3f56d064892b386',
+  },
+  'arbitrum-sepolia': {
+    provider: new ethers.providers.JsonRpcProvider(
+      `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_KEY}`
+    ),
+    explorer: 'https://sepolia.arbiscan.io/',
+    unirepAddress: '0x83cB6AF63eAfEc7998cC601eC3f56d064892b386',
+  },
   // goerli: {
   //   provider: new ethers.providers.JsonRpcProvider(
   //     `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`
@@ -28,20 +35,14 @@ export const NETWORK = {
   //   explorer: 'https://mumbai.polygonscan.com',
   //   unirepAddress: '0x4D137bb44553d55AE6B28B5391c6f537b06C9cc3',
   // },
-  sepolia: {
+  
+  'optimism-sepolia': {
     provider: new ethers.providers.JsonRpcProvider(
-      `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`
+      `https://optimism-sepolia.infura.io/v3/${process.env.INFURA_KEY}`
     ),
-    explorer: 'https://sepolia.etherscan.io',
+    explorer: 'https://sepolia-optimism.etherscan.io/',
     unirepAddress: '0x83cB6AF63eAfEc7998cC601eC3f56d064892b386',
   },
-  // 'optimism-goerli': {
-  //   provider: new ethers.providers.JsonRpcProvider(
-  //     `https://optimism-goerli.infura.io/v3/${process.env.INFURA_KEY}`
-  //   ),
-  //   explorer: 'https://goerli-optimism.etherscan.io/',
-  //   unirepAddress: '0x4D137bb44553d55AE6B28B5391c6f537b06C9cc3',
-  // },
   fuji: {
     provider: new ethers.providers.JsonRpcProvider(
       `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_KEY}`
