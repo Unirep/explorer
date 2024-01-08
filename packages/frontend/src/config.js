fc4d616345c426ea9f69856d2298a1008bb0a316
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-01-08 16:46:33 +0800

    fix(frontend): use process.env

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -2,5 +2,5 @@ export const version = 'v2.0.0'
 export const network = 'sepolia'
 
 // in CI we append a change to this value
-let SERVER = 'http://127.0.0.1:8000'
+let SERVER = process.env.SERVER ?? 'http://127.0.0.1:8000'
 export { SERVER }
