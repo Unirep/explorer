b141c8156c743685138b899b493b921aaf66b463
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-01-11 14:18:06 +0800

    chore: remove debug message

--- scripts/cf_build.sh
+++ scripts/cf_build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "CF_PAGES_BRANCH: $CF_PAGES_BRANCH"
 if [ "$CF_PAGES_BRANCH" = "main" ]; then
     echo "export const version = 'v2.0.0'" > packages/frontend/src/config.js
     echo "export const network = 'sepolia'" >> packages/frontend/src/config.js
@@ -10,5 +9,3 @@ else
     echo "export const network = 'sepolia'" >> packages/frontend/src/config.js
     echo "export const SERVER = 'https://${CF_PAGES_BRANCH}.unirep-explorer-backend.pages.dev'" >> packages/frontend/src/config.js
 fi
-
-cat packages/frontend/src/config.js
