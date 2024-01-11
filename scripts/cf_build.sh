ca67213685ed478d874265f3baaf2310adc11f15
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-01-11 14:13:48 +0800

    fix: fix build script

--- scripts/cf_build.sh
+++ scripts/cf_build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ "$CF_PAGES_BRANCH" = "production" ]; then
+echo "CF_PAGES_BRANCH: $CF_PAGES_BRANCH"
+if [ "$CF_PAGES_BRANCH" = "main" ]; then
     echo "export const version = 'v2.0.0'" > packages/frontend/src/config.js
     echo "export const network = 'sepolia'" >> packages/frontend/src/config.js
     echo "export const SERVER = 'https://unirep-explorer-backend.pages.dev'" >> packages/frontend/src/config.js
-elif [ "$CF_PAGES_BRANCH" = "staging" ]; then
-    branch=$(git branch --show-current)
-    branch="${branch%"${branch##*[![:space:]]}"}" # Trim trailing whitespace
+else
     echo "export const version = 'v2.0.0'" > packages/frontend/src/config.js
     echo "export const network = 'sepolia'" >> packages/frontend/src/config.js
-    echo "export const SERVER = 'https://${branch}.unirep-explorer-backend.pages.dev'" >> packages/frontend/src/config.js
+    echo "export const SERVER = 'https://${CF_PAGES_BRANCH}.unirep-explorer-backend.pages.dev'" >> packages/frontend/src/config.js
 fi
+
+cat packages/frontend/src/config.js
