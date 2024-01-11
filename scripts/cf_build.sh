151f44cda6ac02f62b6831d57f093b5c4baeee50
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-01-11 13:38:57 +0800

    build: add scripts for building backend and frontend

---
+++ scripts/cf_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+source .env
+
+if [ "$CF_PAGES_BRANCH" = "production" ]; then
+    echo "export const version = 'v2.0.0'" > packages/frontend/src/config.js
+    echo "export const network = 'sepolia'" >> packages/frontend/src/config.js
+    echo "export const SERVER = 'https://unirep-explorer-backend.pages.dev'" >> packages/frontend/src/config.js
+elif [ "$CF_PAGES_BRANCH" = "staging" ]; then
+    branch=$(git branch --show-current)
+    branch="${branch%"${branch##*[![:space:]]}"}" # Trim trailing whitespace
+    echo "export const version = 'v2.0.0'" > packages/frontend/src/config.js
+    echo "export const network = 'sepolia'" >> packages/frontend/src/config.js
+    echo "export const SERVER = 'https://${branch}.unirep-explorer-backend.pages.dev'" >> packages/frontend/src/config.js
+fi
